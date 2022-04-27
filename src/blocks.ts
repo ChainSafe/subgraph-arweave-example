3f057a69025b93a7bf7b3d048fc30d8861446c45
clearloop <26088946+clearloop@users.noreply.github.com>
2022-04-28 02:57:00 +0800

    feat(tx): update subgraph example with transaction handler

--- src/blocks.ts
+++ src/blocks.ts
@@ -1,34 +1,6 @@
 import { arweave, BigInt } from "@graphprotocol/graph-ts"
 import { Block, Poa, Tag, Transaction } from "../generated/schema"
 
-export function handleBlock(block: arweave.Block): void {
-  let hash = block.indepHash.toHexString();
-  let entity = new Block(hash);
-
-  entity.height = BigInt.fromU64(block.height);
-  entity.timestamp = BigInt.fromU64(block.timestamp);
-  entity.indep_hash = block.indepHash;
-  entity.nonce = block.nonce;
-  entity.previous_block = block.previousBlock;
-  entity.last_retarget = BigInt.fromU64(block.lastRetarget);
-  entity.diff = block.diff;
-  entity.hash = block.hash;
-  entity.tx_root = block.txRoot;
-  entity.txs = saveTxs(hash, block.txs);
-  entity.wallet_list = block.walletList;
-  entity.reward_addr = block.rewardAddr;
-  entity.tags = saveTags(hash, block.tags);
-  entity.reward_pool = block.rewardPool;
-  entity.weave_size = block.weaveSize;
-  entity.block_size = block.blockSize;
-  entity.cumulative_diff = block.cumulativeDiff;
-  entity.hash_list_merkle = block.hashListMerkle;
-  entity.poa = savePoa(hash, block.poa)
-
-  entity.save()
-}
-
-
 function savePoa(id: string, poa: arweave.ProofOfAccess): string {
   const p = new Poa(id);
 
@@ -43,7 +15,7 @@ function savePoa(id: string, poa: arweave.ProofOfAccess): string {
 }
 
 function saveTags(id: string, tags: arweave.Tag[]): string[] {
-  for (let i =0; i <  tags.length; i ++) {
+  for (let i = 0; i < tags.length; i++) {
     const rawTag = tags[i];
     const tag = new Tag(id);
 
@@ -56,29 +28,48 @@ function saveTags(id: string, tags: arweave.Tag[]): string[] {
   return new Array<string>(tags.length).fill(id);
 }
 
+export function handleBlock(block: arweave.Block): void {
+  let hash = block.indepHash.toHexString();
+  let entity = new Block(hash);
 
-function saveTxs(hash: string, txs: arweave.Transaction[]): string[] {
-  let ids = new Array<string>();
-  for (let i = 0; i < txs.length; i++) {
-    const rawTx = txs[i];
-    const id = rawTx.id.toHexString();
-    const tx = new Transaction(id);
+  entity.height = BigInt.fromU64(block.height);
+  entity.timestamp = BigInt.fromU64(block.timestamp);
+  entity.indep_hash = block.indepHash;
+  entity.nonce = block.nonce;
+  entity.previous_block = block.previousBlock;
+  entity.last_retarget = BigInt.fromU64(block.lastRetarget);
+  entity.diff = block.diff;
+  entity.hash = block.hash;
+  entity.tx_root = block.txRoot;
+  entity.txs = block.txs;
+  entity.wallet_list = block.walletList;
+  entity.reward_addr = block.rewardAddr;
+  entity.tags = saveTags(hash, block.tags);
+  entity.reward_pool = block.rewardPool;
+  entity.weave_size = block.weaveSize;
+  entity.block_size = block.blockSize;
+  entity.cumulative_diff = block.cumulativeDiff;
+  entity.hash_list_merkle = block.hashListMerkle;
+  entity.poa = savePoa(hash, block.poa)
 
-    tx.tx_id = rawTx.id;
-    tx.last_tx = rawTx.lastTx;
-    tx.owner = rawTx.owner;
-    tx.tags = saveTags(rawTx.id.toHexString(), rawTx.tags);
-    tx.data = rawTx.data;
-    tx.data_root = rawTx.dataRoot;
-    tx.data_size = rawTx.dataSize;
-    tx.target = rawTx.target;
-    tx.quantity = rawTx.quantity;
-    tx.signature = rawTx.signature;
-    tx.reward = rawTx.reward;
+  entity.save()
+}
 
-    tx.save();
-    ids.push(id);
-  }
 
-  return ids;
+export function handleTx(tx: arweave.Transaction): void {
+  const entity = new Transaction(tx.id.toHexString());
+
+  entity.tx_id = tx.id;
+  entity.last_tx = tx.lastTx;
+  entity.owner = tx.owner;
+  entity.tags = saveTags(tx.id.toHexString(), tx.tags);
+  entity.data = tx.data;
+  entity.data_root = tx.dataRoot;
+  entity.data_size = tx.dataSize;
+  entity.target = tx.target;
+  entity.quantity = tx.quantity;
+  entity.signature = tx.signature;
+  entity.reward = tx.reward;
+
+  entity.save();
 }
